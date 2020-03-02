bb4c4d3a1d82720a33099cc038a555b71f6fad6d
Gradilenko <artem.gradilenko@gmail.com>
2020-03-02 16:33:36 +0300

    Replaced two buttons with 1 button

--- src/tools/block1.js
+++ src/tools/block1.js
@@ -4,8 +4,7 @@ import xhr from './xhr.js';
 const block1 = document.querySelector('#block1');
 
 const block1Edit = block1.querySelector('#edit');
-const block1Show = block1.querySelector('#show');
-const block1Hide = block1.querySelector('#hide');
+const block1ShowHide = block1.querySelector('#show-hide');
 const block1Cancel = block1.querySelector('#cancel');
 
 const block1Tittle = block1.querySelector('#tittle');
@@ -22,6 +21,7 @@ const pDate = new Datepicker('#date');
 // eslint-disable-next-line no-underscore-dangle
 pDate._opts.onChange = () => {
 	diffBetweenDates(pDate.getDate());
+	block1Date.innerHTML = block1Date.dataset.value;
 };
 
 const sklonenie = (number, txt, cases = [2, 0, 1, 1, 1, 2]) => txt[(number % 100 > 4
@@ -46,6 +46,13 @@ const setValues = () => {
 	block1Tittle.innerHTML = `${vars.person1Name} & ${vars.person2Name}`;
 	block1SubTittle.innerHTML = vars.subTittle;
 	pDate.setDate(vars.date);
+	if (vars.counterShow === true) {
+		block1ShowHide.classList.remove('btn-show');
+		block1ShowHide.classList.add('btn-hide');
+	} else {
+		block1ShowHide.classList.remove('btn-remove');
+		block1ShowHide.classList.add('btn-show');
+	}
 	block1Description.innerHTML = vars.description;
 	block1Hashtag.innerHTML = vars.hashtag;
 };
@@ -55,8 +62,7 @@ setValues();
 block1Edit.addEventListener('click', () => {
 	if (block1Edit.dataset.type === 'edit') {
 		setEditable(true);
-		block1Show.hidden = false;
-		block1Hide.hidden = false;
+		block1ShowHide.hidden = false;
 		block1Cancel.hidden = false;
 		block1Edit.dataset.type = 'save';
 		block1Edit.classList.add('btn-save');
@@ -64,22 +70,25 @@ block1Edit.addEventListener('click', () => {
 		setEditable(false);
 		block1Edit.dataset.type = 'edit';
 		block1Edit.classList.remove('btn-save');
-		block1Show.hidden = true;
-		block1Hide.hidden = true;
+		block1ShowHide.hidden = true;
 		block1Cancel.hidden = true;
 		saveChanges();
 	}
 });
 
-block1Show.addEventListener('click', () => {
-	block1Counter.hidden = false;
-	vars.counterShow = true;
-	diffBetweenDates(pDate.getDate());
-});
-
-block1Hide.addEventListener('click', () => {
-	block1Counter.hidden = true;
-	vars.counterShow = false;
+block1ShowHide.addEventListener('click', () => {
+	if (block1Counter.hidden) {
+		block1ShowHide.classList.remove('btn-show');
+		block1ShowHide.classList.add('btn-hide');
+		block1Counter.hidden = false;
+		vars.counterShow = true;
+		diffBetweenDates(pDate.getDate());
+	} else {
+		block1ShowHide.classList.remove('btn-hide');
+		block1ShowHide.classList.add('btn-show');
+		block1Counter.hidden = true;
+		vars.counterShow = false;
+	}
 });
 
 const showCounter = () => {
@@ -96,8 +105,7 @@ block1Cancel.addEventListener('click', () => {
 
 	block1Edit.dataset.type = 'edit';
 	block1Edit.classList.remove('btn-save');
-	block1Show.hidden = true;
-	block1Hide.hidden = true;
+	block1ShowHide.hidden = true;
 	block1Cancel.hidden = true;
 });
 
